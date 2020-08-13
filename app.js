00e9462d47035b671ea68f5c6182510e0b143019
daniel30-07 <danielborgesdecarvalho@gmail.com>
2020-08-12 22:31:12 -0300

    add mongodb and route new

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ const express = require("express"),
     bodyParser = require("body-parser"),
     app = express();
 
+mongoose.connect('mongodb://localhost:27017/vida_leve', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('Connected to DB!'))
+    .catch(error => console.log(error.message));
 //APP CONFIG
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
 //MONGOOSE/MODEL CONFIG
@@ -15,7 +21,8 @@ const vidaLeveSchema = new mongoose.Schema({
     title: String,
     image: String,
     body: String,
-    created: {type: Date, default: Date.now}
+    category: String,
+    created: { type: Date, default: Date.now }
 });
 const VidaLeve = mongoose.model("Postage", vidaLeveSchema);
 
@@ -23,7 +30,15 @@ const VidaLeve = mongoose.model("Postage", vidaLeveSchema);
 
 //INDEX
 app.get("/", (req, res) => res.redirect("/index"));
-app.get("/index", (req, res) => res.render("index"));
+app.get("/index", (req, res) => {
+    VidaLeve.find({}, (err, postages) => {
+        if(err) {
+            console.log("log...", err);
+        } else {
+        res.render("index", {postages: postages});
+        }  
+    });
+});
 
 // NEW ROUTE
 app.get("/index/new", (req, res) => res.render("new"));
@@ -31,7 +46,7 @@ app.get("/index/new", (req, res) => res.render("new"));
 //CREATE ROUTE  
 app.post("/index", (req, res) => {
     //create blog
-    Postage.create(req.body.postage)
+    VidaLeve.create(req.body.postage)
         .then((newPostage) => res.redirect("/index"))
         .catch((error) => res.render("new"))
 })
